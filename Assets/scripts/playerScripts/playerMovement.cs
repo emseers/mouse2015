f2bd2f87224819d39266b86c11760d48cd6f0ec6
Adipa Wijayathilaka <adipamihi@gmail.com>
2015-11-17 14:00:12 -0500

    Test movement script (wasd, ad rotate)

---
+++ Assets/scripts/playerScripts/playerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+	public float playerSpeed = 0f;
+	public float playerRotateSpeed = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+			this.transform.Translate(0f, 0f,  playerSpeed * Time.deltaTime, Space.Self);
+		else if(Input.GetKey(KeyCode.S))
+	        this.transform.Translate(0f, 0f,  -1 * playerSpeed * Time.deltaTime, Space.Self);
+
+		if (Input.GetKey (KeyCode.D))
+			this.transform.Rotate (0f, playerRotateSpeed * Time.deltaTime, 0f, Space.World);
+		else if (Input.GetKey (KeyCode.A))
+			this.transform.Rotate (0f, -1 * playerRotateSpeed * Time.deltaTime, 0f, Space.World);
+
+	}
+}
