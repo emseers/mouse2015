7be2339cd971a37954c3dec96b25907a11d423be
Arvind Suresh <arvind.suresh60@gmail.com>
2015-11-18 22:24:51 -0500

    Got left and right turn working. (Still need to do forward and back)

--- Assets/scripts/playerScripts/playerMovement.cs
+++ Assets/scripts/playerScripts/playerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class playerMovement : MonoBehaviour {
 	public float playerSpeed = 0f;
 	public float playerRotateSpeed = 0f;
+    public float angleToRotate = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,22 +20,42 @@ public class playerMovement : MonoBehaviour {
 
 		if (Input.GetKey(KeyCode.D)) {
 			if (this.transform.eulerAngles.y != 90) {
-				if (this.transform.eulerAngles.y < 90 || this.transform.eulerAngles.y > 270)
-					this.transform.Rotate(0f, playerRotateSpeed * Time.deltaTime, 0f, Space.World);
-				else
-					this.transform.Rotate(0f, -1 * playerRotateSpeed * Time.deltaTime, 0f, Space.World);
+				if (this.transform.eulerAngles.y < 90 || this.transform.eulerAngles.y > 270) {
+                    if (this.transform.eulerAngles.y < 90)
+                        angleToRotate = 90 - this.transform.eulerAngles.y;
+                    else
+                        angleToRotate = 180 - (this.transform.eulerAngles.y - 270);
+					this.transform.Rotate(0f, angleToRotate / playerRotateSpeed, 0f, Space.World);
+                }
+                else {
+                    if (this.transform.eulerAngles.y < 180)
+                        angleToRotate = 90 - (180 - this.transform.eulerAngles.y);
+                    else
+                        angleToRotate = 90 + (this.transform.eulerAngles.y - 180);
+                    this.transform.Rotate(0f, -1 * angleToRotate / playerRotateSpeed, 0f, Space.World);
+                }
 			}
-			//this.transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f, Space.Self);
-			this.transform.Translate(0f, 0f, playerSpeed * Time.deltaTime, Space.Self);
+			this.transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f, Space.World);
 		} else if (Input.GetKey(KeyCode.A)) {
 			if (this.transform.eulerAngles.y != 270) {
-				if (this.transform.eulerAngles.y < 90 || this.transform.eulerAngles.y > 270)
-					this.transform.Rotate(0f, -1 * playerRotateSpeed * Time.deltaTime, 0f, Space.World);
-				else
-					this.transform.Rotate(0f, playerRotateSpeed * Time.deltaTime, 0f, Space.World);
+                if (this.transform.eulerAngles.y < 90 || this.transform.eulerAngles.y > 270)
+                {
+                    if (this.transform.eulerAngles.y < 90)
+                        angleToRotate = 180 - (90 - this.transform.eulerAngles.y);
+                    else
+                        angleToRotate = this.transform.eulerAngles.y - 270;
+                    this.transform.Rotate(0f, -1 * angleToRotate / playerRotateSpeed, 0f, Space.World);
+                }
+                else
+                {
+                    if (this.transform.eulerAngles.y < 180)
+                        angleToRotate = 90 + (180 - this.transform.eulerAngles.y);
+                    else
+                        angleToRotate = 90 - (this.transform.eulerAngles.y - 180);
+                    this.transform.Rotate(0f, angleToRotate / playerRotateSpeed, 0f, Space.World);
+                }
 			}
-			//this.transform.Translate(-1 * playerSpeed * Time.deltaTime, 0f, 0f, Space.Self);
-			this.transform.Translate(0f, 0f, -1 * playerSpeed * Time.deltaTime, Space.Self);
+			this.transform.Translate(-1 * playerSpeed * Time.deltaTime, 0f, 0f, Space.World);
 		}
 	}
 }
