a90987eace17aa17ef0aab5425e77b904131c23e
boarnoah <adipamihi@gmail.com>
2015-11-18 23:27:18 -0500

    Added pause menu and rudimentary pause (by stopping time tick)

---
+++ Assets/scripts/gameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour {
+    public GameObject pauseCanvas;
+    public bool isPaused = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused){
+                isPaused = false;
+                Time.timeScale = 1f;
+                pauseCanvas.SetActive(false);
+            }else {
+                isPaused = true;
+                Time.timeScale = 0f;
+                pauseCanvas.SetActive(true);
+            }
+        }
+	}
+}
