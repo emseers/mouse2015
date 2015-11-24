60d4b31f3a75202b014bab24fb359458d6c92349
Arvind Suresh <arvind.suresh60@gmail.com>
2015-11-23 21:53:18 -0500

    Added basic logic for moving camera position based on player position. Changing rotation will require more thinking...

--- Assets/scripts/cameraScripts/cameraMovement.cs
+++ Assets/scripts/cameraScripts/cameraMovement.cs
@@ -5,42 +5,51 @@ public class cameraMovement : MonoBehaviour {
     public GameObject cameraPath = null;
     public GameObject playerObject = null;
     public int currentNode = 0;
-
     public float testSpeed = 0f;
-    private Vector3 nextNodeV;
-    private Vector3 nodeRail;
+
+    private Vector3 cameraPosition;
+    private Vector3 cameraRotation;
     private float testMove = 0f;
 
-    private Vector3 testV;
+    private Vector3 playerPosition;
+    private float ratio;
 
 	// Use this for initialization
 	void Start () {
-        nextNodeV = getNodeV(currentNode + 1);
-        nodeRail = nextNodeV - getNodeV(currentNode);
 
-        testV = transform.position;
-
-        
-        //Debug.Log(testV + " ========================== " + nextNodeV);
-        //for(int derp = 0; derp < cameraPath.transform.childCount; derp++)
-        //    Debug.Log(derp + " NODE: " + cameraPath.transform.GetChild(derp) + " ===>  " + cameraPath.transform.GetChild(derp).position);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.Lerp(testV, nextNodeV, 0.9f);
-        //TODO: Fancy movement along "spline" in sync to player position or w/e
-        /*
-        
-        */
-
-
+        playerPosition = playerObject.transform.position;
+        if (playerPosition.z <= 0) {
+            this.transform.position = getPositionByNode(0);
+        } else if (playerPosition.z < 10) {
+            ratio = playerPosition.z / 10.0f;
+            cameraPosition = getPositionByNode(1) - getPositionByNode(0);
+            cameraPosition *= ratio;
+            cameraRotation = getRotationByNode(1) - getRotationByNode(0);
+            cameraRotation *= ratio;
+            this.transform.position = getPositionByNode(0) + cameraPosition;
+            //this.transform.eulerAngles = getRotationByNode(0) + cameraRotation;
+        } else if (playerPosition.z < 25) {
+            ratio = (playerPosition.z - 10) / (25.0f - 10);
+            cameraPosition = getPositionByNode(2) - getPositionByNode(1);
+            cameraPosition *= ratio;
+            cameraRotation = getRotationByNode(2) - getRotationByNode(1);
+            cameraRotation *= ratio;
+            this.transform.position = getPositionByNode(1) + cameraPosition;
+            //this.transform.eulerAngles = getRotationByNode(1) + cameraRotation;
+        }
     }
 
-    Vector3 getNodeV(int nodeNumber) {
-        //Vector3 herp = cameraPath.transform.GetChild(nodeNumber).position;
-        //Debug.Log(herp);
-        //return herp;
+    // Returns a vector containing the specified camera node position
+    Vector3 getPositionByNode (int nodeNumber) {
         return cameraPath.transform.GetChild(nodeNumber).position;
+    }
+
+    // Returns a vector containing the specified camera node rotation
+    Vector3 getRotationByNode(int nodeNumber) {
+        return cameraPath.transform.GetChild(nodeNumber).eulerAngles;
     }
 }
