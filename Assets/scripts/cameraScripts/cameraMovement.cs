e160003b250510c7c054756d9666b939ee7e71a9
Arvind Suresh <arvind.suresh60@gmail.com>
2015-12-14 21:11:57 -0500

    Finally got linking between playerPath nodes and cameraPath nodes working properly with all possible player positions (I overlooked a relatively simple way of doing it, silly me). Also attempted to comment the big mess of my code that I made.

--- Assets/scripts/cameraScripts/cameraMovement.cs
+++ Assets/scripts/cameraScripts/cameraMovement.cs
@@ -5,28 +5,36 @@ public class cameraMovement : MonoBehaviour {
 	public GameObject cameraPath = null;
 	public GameObject playerPath = null;
 	public GameObject playerObject = null;
-	public int currentNode = 0;
-	public float testSpeed = 0f;
 
-	private Vector3 cameraPosition;
-	private Vector3 cameraRotation;
-	private float testMove = 0f;
+	//IMPORTANT: Make sure cameraPath and playerPath have same number of children,
+	//and update this in the Unity Editor to be the correct number
+	public int nodes = 0;
 
-	private Vector3 playerPosition;
-	private float ratio;
+	private Vector3 cameraPosition; //A vector to help calculate camera position
+	private Vector3 cameraRotation; //A vector to help calculate camera rotation
+
+	private Vector3 playerPosition; //A vector to store player position
+	private float ratio; //A variable to store how far in between two nodes the player is
 
 	// Use this for initialization
 	void Start() {
-
+		this.transform.position = getPositionByNode(0); //Set the initial position to the first node (duh)
 	}
 
 	// Update is called once per frame
 	void Update() {
 		playerPosition = playerObject.transform.position;
-		float[] distances = new float[3];
-		float minDistance = getPositionBetweenNodes(1, 2); //Initialize minimum to first distance
-		int instance = 0; //Variable to store which instance the player is closest to
-		for (int i = 0; i < 2; i++) {
+
+		//An array to store distances between all possible nodes (to see which one is minimum)
+		float[] distances = new float[nodes];
+
+		//A variable to store the minimum distance. Initialize to first distance
+		float minDistance = getPositionBetweenNodes(0, 1);
+
+		//A variable to store in which instance the distance was the smallest
+		int instance = 0;
+
+		for (int i = 0; i < nodes - 1; i++) {
 			distances[i] = getPositionBetweenNodes(i, i + 1); //Set distances for all instances
 			if (distances[i] <= minDistance) {
 				minDistance = distances[i]; //Store the minimum distance for comparison
@@ -35,28 +43,11 @@ public class cameraMovement : MonoBehaviour {
 		}
 
 		distances[instance] = getPositionBetweenNodes(instance, instance + 1); //Re-run function to store correct ratio;
+		
+		//Some vector math to set the correct camera position based on ratio
 		cameraPosition = getPositionByNode(instance + 1) - getPositionByNode(instance);
 		cameraPosition *= ratio;
 		this.transform.position = getPositionByNode(instance) + cameraPosition;
-		//if (playerPosition.z <= 0) {
-		//	this.transform.position = getPositionByNode(0);
-		//} else if (playerPosition.z < 10) {
-		//	ratio = playerPosition.z / 10.0f;
-		//	cameraPosition = getPositionByNode(1) - getPositionByNode(0);
-		//	cameraPosition *= ratio;
-		//	cameraRotation = getRotationByNode(1) - getRotationByNode(0);
-		//	cameraRotation *= ratio;
-		//	this.transform.position = getPositionByNode(0) + cameraPosition;
-		//	//this.transform.eulerAngles = getRotationByNode(0) + cameraRotation;
-		//} else if (playerPosition.z < 25) {
-		//	ratio = (playerPosition.z - 10) / (25.0f - 10);
-		//	cameraPosition = getPositionByNode(2) - getPositionByNode(1);
-		//	cameraPosition *= ratio;
-		//	cameraRotation = getRotationByNode(2) - getRotationByNode(1);
-		//	cameraRotation *= ratio;
-		//	this.transform.position = getPositionByNode(1) + cameraPosition;
-		//	//this.transform.eulerAngles = getRotationByNode(1) + cameraRotation;
-		//}
 	}
 
 	// Returns a vector containing the specified camera node position
@@ -70,24 +61,35 @@ public class cameraMovement : MonoBehaviour {
 	}
 
 	//Sets the ratio to a number between 0 and 1 on how far along the player is between two specified player nodes
-	//and returns the distance from the player to the line connecting the two nodes
 	float getPositionBetweenNodes(int node1, int node2) {
+		//A vector going from node1 to node2
 		Vector3 nodeVector = new Vector3(playerPath.transform.GetChild(node2).transform.position.x -
 										playerPath.transform.GetChild(node1).transform.position.x,
 										playerPath.transform.GetChild(node2).transform.position.y -
 										playerPath.transform.GetChild(node1).transform.position.y,
 										playerPath.transform.GetChild(node2).transform.position.z -
 										playerPath.transform.GetChild(node1).transform.position.z);
+		
+		//A vector going from node1 to player position
 		Vector3 playerVector = new Vector3(playerPosition.x - playerPath.transform.GetChild(node1).transform.position.x,
 										playerPosition.y - playerPath.transform.GetChild(node1).transform.position.y,
 										playerPosition.z - playerPath.transform.GetChild(node1).transform.position.z);
+		
+		//A vector projection of the player vector projected onto the node vector
 		Vector3 projection = Vector3.Project(playerVector, nodeVector);
-		ratio = projection.magnitude / nodeVector.magnitude;
-		float distance = (playerVector - projection).magnitude;
-		if (ratio > 1)
+		
+		ratio = projection.magnitude / nodeVector.magnitude; //Calculate ratio with projection
+		float distance = (playerVector - projection).magnitude; //Calculate distance with projection
+		
+		//Base cases if the projection doesn't actually fall on the node vector (if it's too big or in the opposite direction)
+		if (ratio > 1) {
 			ratio = 1;
-		else if (nodeVector.normalized != projection.normalized)
+			distance = (playerVector - nodeVector).magnitude;
+		} else if (nodeVector.normalized != projection.normalized) {
 			ratio = 0;
+			distance = playerVector.magnitude;
+		}
+
 		return distance;
 	}
 }
